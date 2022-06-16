b6ac4ff60ebb39e13127060a69a2f875482a4a8c
Amoriss <amoriss@protonmail.com>
2022-06-15 23:15:33 -0500

    Create DemoLogic.cs

---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,12 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic
+{
+    public int Value1 { get; private set; } 
+    public int Value2 { get; private set; }
+    public DemoLogic()
+    {
+        Value1 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+        Value1 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+    }
+}
