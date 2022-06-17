d3103c92bdebfdd1e4e92176786fe56592129245
Amoriss <amoriss@protonmail.com>
2022-06-16 23:41:20 -0500

    DI using AddSingleton

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddTransient<DemoLogic>();
+builder.Services.AddSingleton<DemoLogic>();
 
 var app = builder.Build();
 
