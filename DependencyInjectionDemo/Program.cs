c8b6a6f35ea1e95f8e44d4047f7038cf539db655
Amoriss <amoriss@protonmail.com>
2022-06-16 23:37:30 -0500

    implement dependency injection

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using DependencyInjectionDemo.Logic;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddTransient<DemoLogic>();
 
 var app = builder.Build();
 
